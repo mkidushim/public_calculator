ad3f96c3cbb6daeaef0d7d2eae9e1ae38c240356
mkidushim <mkidushim@gmail.com>
2015-05-04 17:48:23 -0700

    jsfiddle put into index.html and js

---
+++ calc.js
@@ -0,0 +1,19 @@
+function add_numbers() {
+    var zero = $('#number_0').val();
+    var one = $('#number_1').val();
+    var result = (parseInt(zero) + parseInt(one));
+    console.log('result:', result)
+    $('#result_display').html(result);
+    $('#operator').text('+');
+
+}
+
+function subtract() {
+
+    var zero = $('#number_0').val();
+    var one = $('#number_1').val();
+    var result = (parseInt(zero) - parseInt(one));
+
+    $('#result_display').html(result);
+    $('#operator').text('-');
+}
