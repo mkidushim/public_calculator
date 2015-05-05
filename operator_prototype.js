6665b1823b13ba55df461c3fef35c4d1b7ffbe99
mkidushim <mkidushim@gmail.com>
2015-05-05 10:44:22 -0700

    Finished Prototype 0

--- operator_prototype.js
+++ operator_prototype.js
@@ -2,7 +2,7 @@ function add_numbers() {
     var zero = $('#number_0').val();
     var one = $('#number_1').val();
     var result = (parseInt(zero) + parseInt(one));
-    console.log('result:', result)
+    console.log('result:', result);
     $('#result_display').html(result);
     $('#operator').text('+');
 
@@ -13,7 +13,7 @@ function subtract() {
     var zero = $('#number_0').val();
     var one = $('#number_1').val();
     var result = (parseInt(zero) - parseInt(one));
-
+    console.log('result:', result);
     $('#result_display').html(result);
     $('#operator').text('-');
 }
@@ -22,6 +22,7 @@ function multiply() {
     var zero = $('#number_0').val();
     var one = $('#number_1').val();
     var result = (parseInt(zero) * parseInt(one));
+    console.log('result:', result);
     $('#result_display').html(result);
     $('#operator').text('*')
 }
@@ -30,6 +31,7 @@ function divide() {
     var zero = $('#number_0').val();
     var one = $('#number_1').val();
     var result = (parseInt(zero) / parseInt(one));
+    console.log('result:', result);
     $('#result_display').html(result);
     $('#operator').text('/')
 }
